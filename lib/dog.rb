4b4a063c993d8a62913eec2661edd264fe04251e
Amer Fahmy <afahmy519@gmail.com>
2020-05-12 19:03:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,15 @@
 class Dog
  
- def dog_name(name)
+ def initialize(name)
    @name = name
  end
  
  def new_name=(name)
    @name = name
+ end
+ 
+ def name
+   @name
  end
  
  def dog_breed(breed)
